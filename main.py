aaa2dad1e497fbab86660a7ce714385942cbf027
Triceratops <75021575+joshrkhoo@users.noreply.github.com>
2023-06-14 18:54:59 +1000

    jsonify elements within each todo object

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+CORS(app)
 from monogodb import get_todos
+
 
 @app.route("/todos")
 def todos():
     todos = get_todos()
-    return str(todos)
+    todo_list = []
+    for todo in todos:
+        todo_dict = {
+            'id': str(todo['_id']),
+            'title': todo['title'],
+            'description': todo['description']
+        }
+        todo_list.append(todo_dict)
+    return jsonify(todo_list)
