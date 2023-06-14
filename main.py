ab26f07fd0dd968606cde4b1f51f1d9b55be6958
Triceratops <75021575+joshrkhoo@users.noreply.github.com>
2023-06-14 18:42:57 +1000

    stuff

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 app = Flask(__name__)
-
-app.config["MONGO_URI"] ="mongodb+srv://giganotosaurus:Joshpass01@todo-api.kwzhjvd.mongodb.net/development?retryWrites=true&w=majority"
-
-mongo = PyMongo(app)
-mongo.init_app(app)
-
-print(mongo)
-print(vars(mongo))
-
-def get_todos():
-    elements = mongo.db.todos.find()
-    print(elements)
-    return list(elements)
-
+from monogodb import get_todos
 
 @app.route("/todos")
 def todos():
--- monogodb.py
+++ monogodb.py
@@ -1,15 +1,15 @@
-# from flask_pymongo import PyMongo
-# from main import app
+from flask_pymongo import PyMongo
+from main import app
 
-# app.config["MONGO_URI"] ="mongodb+srv://giganotosaurus:Joshpass01@todo-api.kwzhjvd.mongodb.net/?retryWrites=true&w=majority"
+app.config["MONGO_URI"] ="mongodb+srv://giganotosaurus:Joshpass01@todo-api.kwzhjvd.mongodb.net/development?retryWrites=true&w=majority"
 
-# mongo = PyMongo(app)
-# mongo.init_app(app)
+mongo = PyMongo(app)
+mongo.init_app(app)
 
-# print(mongo)
-# print(vars(mongo))
+print(mongo)
+print(vars(mongo))
 
-# def get_todos():
-#     elements = mongo.db.todos.find()
-#     print(elements)
-#     return list(elements)
+def get_todos():
+    elements = mongo.db.todos.find()
+    print(elements)
+    return list(elements)
